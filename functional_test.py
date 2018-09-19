c608ff017df4d24d3654c78ea795f59b08dfe290
granaderos <marejeanperpinosa@gmail.com>
2018-09-19 17:42:15 +0800

    Function chec_for_row_in_list_table created. DRY was implemented.

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -27,16 +32,14 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
-        input.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         #time.sleep(10)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
@@ -44,4 +47,3 @@ class NewVisitorTest(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
 
-    
