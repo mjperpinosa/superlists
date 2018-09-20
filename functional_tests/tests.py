63f5cfd3d055e513bf149f8c0464a5d611bd2823
granaderos <marejeanperpinosa@gmail.com>
2018-09-20 14:11:55 +0800

    make functional_tests an app, use LiveServerTestCase

---
+++ functional_tests/tests.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
+import unittest
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        # inputbox1 = self.browser.find_element_by_id('id_new_item')
+        # inputbox1.send_keys('Use peacock feathers to make a fly')
+        # inputbox1.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table("1: Buy peacock feathers")        
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
+        self.fail('Finish the test!')
+
+
+# if __name__ == '__main__':
+#     unittest.main(warnings='ignore')
+
