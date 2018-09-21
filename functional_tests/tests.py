f058d21958ee39637f8000fa798ca92ff8ff8e8e
granaderos <marejeanperpinosa@gmail.com>
2018-09-21 15:15:16 +0800

    New URl, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.expected_conditions import staleness_of
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
 import unittest
 
@@ -10,7 +12,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(20)
 
     def tearDown(self):
         self.browser.quit()
@@ -23,6 +25,7 @@ class NewVisitorTest(LiveServerTestCase):
         )
 
     def check_for_row_in_list_table(self, row_text):
+        #with self.wait_for_page_load(timeout=10):
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
         self.assertIn(row_text, [row.text for row in rows])
@@ -34,20 +37,21 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
-
-        with self.wait_for_page_load(timeout=10):
-            self.check_for_row_in_list_table("1: Buy peacock feathers")  
-
+        #with self.wait_for_page_load(10):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+
+        # inputbox = self.browser.find_element_by_id('id_new_item')
+        # inputbox.send_keys('Buy peacock feathers')
+        # inputbox.send_keys(Keys.ENTER)
+
+        
+
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, "/lists/.+")
 
-        with self.wait_for_page_load(timeout=10):
+        with self.wait_for_page_load():
             self.check_for_row_in_list_table("1: Buy peacock feathers")        
             self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
